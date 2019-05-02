b968d95fa5103a36027728d61bb9a24a8358ddff
nachoMorera <ijmorera19@gmail.com>
2019-05-02 18:19:58 +0200

    listeners buttons

--- custom.js
+++ custom.js
@@ -1,3 +1,7 @@
+var red_marker, blue_marker, green_marker, yellow_marker;
+var red_infowindow, blue_infowindow, green_infowindow, yellow_infowindow;
+var map;
+
 $(function () {
     
     function initMap() {
@@ -12,11 +16,11 @@ $(function () {
             scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-        var map = new google.maps.Map(mapCanvas, mapOptions);
+        map = new google.maps.Map(mapCanvas, mapOptions);
 
         // Red marker -----------------
 
-        var red_marker = new google.maps.Marker({
+        red_marker = new google.maps.Marker({
             position: new google.maps.LatLng(41.3863738,2.1757399),
             map: map,
             icon: 'images/red_marker.png'
@@ -29,14 +33,14 @@ $(function () {
                 '</div>' +
                 '</div>';
 
-        var red_infowindow = new google.maps.InfoWindow({
+        red_infowindow = new google.maps.InfoWindow({
             content: red_contentString,
             maxWidth: 400
         });
 
 		// Blue marker -----------------
 
-        var blue_marker = new google.maps.Marker({
+	blue_marker = new google.maps.Marker({
             position: new google.maps.LatLng(41.3863337,2.1630204),
             map: map,
             icon: 'images/blue_marker.png'
@@ -49,7 +53,7 @@ $(function () {
                 '</div>' +
                 '</div>';
 
-        var blue_infowindow = new google.maps.InfoWindow({
+        blue_infowindow = new google.maps.InfoWindow({
             content: blue_contentString,
             maxWidth: 400
         });
@@ -60,7 +64,7 @@ $(function () {
 
 		// Yellow marker -----------------
 
-        var yellow_marker = new google.maps.Marker({
+        yellow_marker = new google.maps.Marker({
             position: new google.maps.LatLng(41.384035, 2.1656359),
             map: map,
             icon: 'images/yellow_marker.png'
@@ -73,14 +77,14 @@ $(function () {
                 '</div>' +
                 '</div>';
 
-        var yellow_infowindow = new google.maps.InfoWindow({
+        yellow_infowindow = new google.maps.InfoWindow({
             content: yellow_contentString,
             maxWidth: 400
         });
 
 		// Green marker -----------------
 
-        var green_marker = new google.maps.Marker({
+        green_marker = new google.maps.Marker({
             position: new google.maps.LatLng(41.3862477, 2.1701107),
             map: map,
             icon: 'images/green_marker.png'
@@ -93,7 +97,7 @@ $(function () {
                 '</div>' +
                 '</div>';
 
-        var green_infowindow = new google.maps.InfoWindow({
+        green_infowindow = new google.maps.InfoWindow({
             content: green_contentString,
             maxWidth: 400
         });
@@ -127,7 +131,157 @@ $(function () {
             yellow_infowindow.close(map, yellow_marker);
             green_infowindow.open(map, green_marker);
         });
+	
     }
 
     google.maps.event.addDomListener(window, 'load', initMap);
+    
+    //cuando aprietas un botón se centra el amrcador correspondiente
+	
+    var medi_ambient  = document.getElementById("medi_ambient");
+    var serveis = document.getElementById("serveis");
+    var socials = document.getElementById("socials");
+    var infraestructures = document.getElementById("infraestructures");
+
+    serveis.addListener('click', function () {
+            red_infowindow.open(map, red_marker);
+            blue_infowindow.close(map, blue_marker);
+            yellow_infowindow.close(map, yellow_marker);
+            green_infowindow.close(map, green_marker);
+        });
+
+        serveis.addListener('click', function () {
+            red_infowindow.close(map, red_marker);
+            blue_infowindow.open(map, blue_marker);
+            yellow_infowindow.close(map, yellow_marker);
+            green_infowindow.close(map, green_marker);
+        });
+
+        socials.addListener('click', function () {
+            red_infowindow.close(map, red_marker);
+            blue_infowindow.close(map, blue_marker);
+            yellow_infowindow.open(map, yellow_marker);
+            green_infowindow.close(map, green_marker);
+        });
+
+        infraestructures.addListener('click', function () {
+            red_infowindow.close(map, red_marker);
+            blue_infowindow.close(map, blue_marker);
+            yellow_infowindow.close(map, yellow_marker);
+            green_infowindow.open(map, green_marker);
+        });
 });
+
+var inicio = 0;
+		
+		function esconder() {
+			document.getElementById("medi_ambient").style.display = "none";
+			document.getElementById("serveis").style.display = "none";
+			document.getElementById("socials").style.display = "none";
+			document.getElementById("infraestructures").style.display = "none";
+			
+			document.getElementById("medi_ambient2").style.display = "block";
+			document.getElementById("serveis2").style.display = "block";
+			document.getElementById("socials2").style.display = "block";
+			document.getElementById("infraestructures2").style.display = "block";
+			inicio = 1;
+		}
+		
+		function myFunction1() {
+			var y = document.getElementById("medi_ambient2");
+			var x = document.getElementById("medi_ambient");
+			if(inicio == 0) {
+				esconder();
+				red_marker.setVisible(false);
+				red_infowindow.close(map, red_marker);
+				blue_marker.setVisible(false);
+				blue_infowindow.close(map, blue_marker);
+				yellow_marker.setVisible(false);
+				yellow_infowindow.close(map, yellow_marker);
+				
+			}
+			if (x.style.display === "none") {
+				x.style.display = "block";
+				y.style.display = "none";
+				green_marker.setVisible(true);
+			} else {
+				x.style.display = "none";
+				y.style.display = "block";
+				green_marker.setVisible(false);
+				green_infowindow.close(map, green_marker);
+			}
+		}
+
+		function myFunction2() {
+			var y = document.getElementById("serveis2");
+			var x = document.getElementById("serveis");
+			if(inicio == 0) {
+				esconder();
+				red_marker.setVisible(false);
+				red_infowindow.close(map, red_marker);
+				green_marker.setVisible(false);
+				green_infowindow.close(map, green_marker);
+				yellow_marker.setVisible(false);
+				yellow_infowindow.close(map, yellow_marker);
+			}
+			if (x.style.display === "none") {
+				x.style.display = "block";
+				y.style.display = "none";
+				blue_marker.setVisible(true);
+
+			} else {
+				x.style.display = "none";
+				y.style.display = "block";
+				blue_marker.setVisible(false);
+				blue_infowindow.close(map, blue_marker);
+			}
+		}
+
+		function myFunction3() {
+			var y = document.getElementById("socials2");
+			var x = document.getElementById("socials");
+			if(inicio == 0) {
+				esconder();
+				green_marker.setVisible(false);
+				green_infowindow.close(map, green_marker);
+				blue_marker.setVisible(false);
+				blue_infowindow.close(map, blue_marker);
+				yellow_marker.setVisible(false);
+				yellow_infowindow.close(map, yellow_marker);
+			}
+			if (x.style.display === "none") {
+				x.style.display = "block";
+				y.style.display = "none";
+				red_marker.setVisible(true);
+			} else {
+				x.style.display = "none";
+				y.style.display = "block";
+				yellow_marker.setVisible(false);
+				yellow_infowindow.close(map, yellow_marker);
+			}
+		}
+
+		function myFunction4() {
+			var x = document.getElementById("infraestructures");
+			var y = document.getElementById("infraestructures2");
+			if(inicio == 0) {
+				esconder();
+				red_marker.setVisible(false);
+				red_infowindow.close(map, red_marker);
+				blue_marker.setVisible(false);
+				blue_infowindow.close(map, blue_marker);
+				green_marker.setVisible(false);
+				green_infowindow.close(map, green_marker);
+			}
+			if (x.style.display === "none") {
+				y.style.display = "none";
+				x.style.display = "block";
+				yellow_marker.setVisible(true);
+				
+			} else {
+				x.style.display = "none";
+				y.style.display = "block";
+				yellow_marker.setVisible(false);
+				yellow_infowindow.close(map, yellow_marker);
+			}
+		}
